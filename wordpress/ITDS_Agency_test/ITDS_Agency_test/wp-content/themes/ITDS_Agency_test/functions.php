79a2ad8b7d9c0f5403bba98794b1054deb66fc84
Romron <Romron@ukr.net>
2022-07-22 16:38:33 +0300

    что то

--- wordpress/ITDS_Agency_test/ITDS_Agency_test/wp-content/themes/ITDS_Agency_test/functions.php
+++ wordpress/ITDS_Agency_test/ITDS_Agency_test/wp-content/themes/ITDS_Agency_test/functions.php
@@ -1,5 +1,5 @@
 <?php 
-
+require_once ABSPATH . '/wp-admin/includes/taxonomy.php';
 
 
 function ITDS_Agency_test() {
@@ -23,34 +23,45 @@ function ITDS_Agency_test() {
    
 //    wp_enqueue_script( 'main', get_template_directory_uri() . '/assets/js/main.js', array(), '20151215', true );
 }
-
 add_action( 'wp_enqueue_scripts', 'ITDS_Agency_test' );
 show_admin_bar(false);
 
-
-function ITDS_Agency_test_posttype_product_card() {
-   register_post_type( 'product-card***',
-       array(
-           'labels' => array(
-           'name' => __( 'Карточки продуктов' ),
-           'singular_name' => __( 'Карточка продукта' )
-       ),
-       'public' => true,
-       'has_archive' => true,
-       'rewrite' => array('slug' => 'product-card'),
-       )
-   );
-}
-add_action( 'init', 'ITDS_Agency_test_posttype_product_card' );
-
-
-add_action('add_meta_boxes', 'my_extra_fields', 1);
-
-function my_extra_fields() {
-	add_meta_box( 'extra_fields', 'Дополнительные поля', 'extra_fields_box_func', 'post', 'normal', 'high'  );
-}
-
-
-
 add_theme_support( 'post-thumbnails' );
 add_theme_support( 'post-thumbnails', array( 'category' ) );
+
+
+
+// function true_expanded_request_category($q) {
+// 	if (isset($q['product-card'])) // если в запросе присутствует параметр рубрики
+// 		$q['post_type'] = array('product-card', 'page'); // то, помимо записей, выводим также и страницы
+// 	return $q;
+// }
+// add_filter('request', 'true_expanded_request_category');
+
+
+
+// wp_create_category('Товар');
+
+// function ITDS_Agency_test_posttype_product_card() {
+//    register_post_type( 'product-card***',
+//        array(
+//            'labels' => array(
+//            'name' => __( 'Карточки продуктов' ),
+//            'singular_name' => __( 'Карточка продукта' )
+//        ),
+//        'public' => true,
+//        'has_archive' => true,
+//        'rewrite' => array('slug' => 'product-card'),
+//        )
+//    );
+// }
+// add_action( 'init', 'ITDS_Agency_test_posttype_product_card' );
+
+
+// add_action('add_meta_boxes', 'my_extra_fields', 1);
+
+// function my_extra_fields() {
+// 	add_meta_box( 'extra_fields', 'Дополнительные поля', 'extra_fields_box_func', 'post', 'normal', 'high'  );
+// }
+
+
