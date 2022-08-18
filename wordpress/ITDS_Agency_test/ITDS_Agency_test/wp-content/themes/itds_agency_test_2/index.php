f30afccd657f7c4ca6773d0d219c89a71efb4000
Romron <Romron@ukr.net>
2022-08-18 15:29:08 +0300

    что то

--- wordpress/ITDS_Agency_test/ITDS_Agency_test/wp-content/themes/itds_agency_test_2/index.php
+++ wordpress/ITDS_Agency_test/ITDS_Agency_test/wp-content/themes/itds_agency_test_2/index.php
@@ -1,57 +1,138 @@
-<?php
-
-/**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- */
-
-get_header();
-?>
+<?php get_header('/parts/header.php'); ?>
 
 
+<main>
+	<div class="intro">
+		<?php global $ITDS_Agency_options; ?>
+		<div class="intro__foto">
+			<img src="<?php echo esc_url($ITDS_Agency_options['intro_foto']['url']); ?>" alt="">
+			<div class="intro-main-block">
+				<div class="block__logo">
+					<img src="img/logo.png" alt="logo">
+				</div>
+				<div class="intro-main-block__text">Вдохновляем Вас преобразить свой дом</div>
+				<div class="slogan-block">Встигни придбати</div>
+			</div>
+		</div>
+		<div class="wrap-intro-block">
+			<div class="intro-block">
+				<div class="intro-block__text-wrap">
+					<div class="intro-block__title"><?php esc_html_e($ITDS_Agency_options['intro_block-1__title'], 'ITDS_Agency_test'); ?></div>
+					<div class="intro-block__text"><?php esc_html_e($ITDS_Agency_options['intro_block-1__text'], 'ITDS_Agency_test'); ?></div>
+				</div>
+				<div class="intro-block__img">
+					<a href="#"><img src="img/intro-icon-harant.png" alt="icon harant"></a>
+				</div>
+			</div>
+			<div class="intro-block">
+				<div class="intro-block__text-wrap">
+					<div class="intro-block__title"><?php esc_html_e($ITDS_Agency_options['intro_block-2__title'], 'ITDS_Agency_test'); ?></div>
+					<div class="intro-block__text"><?php esc_html_e($ITDS_Agency_options['intro_block-2__text'], 'ITDS_Agency_test'); ?></div>
+				</div>
+				<div class="intro-block__img">
+					<a href="#"><img src="img/intro-icon-viber.png" alt="icon veber"></a>
+				</div>
+			</div>
+			<div class="intro-block">
+				<div class="intro-block__text-wrap">
+					<div class="intro-block__title"><?php esc_html_e($ITDS_Agency_options['intro_block-3__title'], 'ITDS_Agency_test'); ?></div>
+					<div class="intro-block__text"><?php esc_html_e($ITDS_Agency_options['intro_block-3__text'], 'ITDS_Agency_test'); ?></div>
+				</div>
+				<div class="intro-block__img">
+					<a href="#"><img src="img/intro-icon-parcel.png" alt="icon parcel"></a>
+				</div>
+			</div>
+			<div class="intro-block">
+				<div class="intro-block__text-wrap">
+					<div class="intro-block__title"><?php esc_html_e($ITDS_Agency_options['intro_block-4__title'], 'ITDS_Agency_test'); ?></div>
+					<div class="intro-block__text"><?php esc_html_e($ITDS_Agency_options['intro_block-4__text'], 'ITDS_Agency_test'); ?></div>
+				</div>
+				<div class="intro-block__img">
+					<a href="#"><img src="img/intro-icon-fast-delivery.png" alt="icon fast delivery"></a>
+				</div>
+			</div>
+		</div>
+	</div>
 
-<main id="primary" class="site-main">
+	<div class="popular-goods">
+		<div class="popular-goods__row">
+			<div class="popular-goods__title">популярные товары</div>
+			<div class="arow-block">
+				<div class="arow-block__arow-left"><img src="img/arow-block__arow-left.png" alt=""></div>
+				<div class="arow-block__arow-right"><img src="img/arow-block__arow-right.png" alt=""></div>
+			</div>
+		</div>
+		<div class="popular-goods__cards-block">
+			<?php
+			if (have_posts()) : while (have_posts()) : the_post(); ?>
+					<?php get_template_part('parts/product-card'); ?>
+				<?php endwhile;
+			else : ?>
+				постов нет
+			<?php endif;
+			wp_reset_postdata(); ?>
 
-	<?php
-	if (have_posts()) :
+		</div>
+	</div>
 
-		if (is_home() && !is_front_page()) :
-	?>
-			<header>
-				<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-			</header>
-	<?php
-		endif;
+</main>
 
-		/* Start the Loop */
-		while (have_posts()) :
-			the_post();
+<div class="wrap-novelties">
+	<div class="novelties">
+		<div class="novelties__row">
+			<div class="novelties__title">Новинки</div>
+			<div class="arow-block">
+				<div class="arow-block__arow-left fon-white"><img src="img/arow-block__arow-left-green.png" alt=""></div>
+				<div class="arow-block__arow-right fon-white"><img src="img/arow-block__arow-right-green.png" alt=""></div>
+			</div>
+		</div>
+		<div class="novelties__cards-block">
 
-			/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-			get_template_part('template-parts/content', get_post_type());
+			<?php $args = array(
+				'post_type' => 'goods',
+				'posts_per_page' => 4,
+			);
+			$goods = new WP_Query($args);
+			if ($goods->have_posts()) : while ($goods->have_posts()) : $goods->the_post(); ?>
+					<?php get_template_part('parts/product-card'); ?>
+				<?php endwhile;
+			else : ?>
+				постов нет
+			<?php endif; ?>
 
-		endwhile;
 
-		the_posts_navigation();
+		</div>
+	</div>
+</div>
 
-	else :
+<div class="instagram">
+	<?php get_template_part('parts/instagram-tape'); ?>
+</div>
 
-		get_template_part('template-parts/content', 'none');
 
-	endif;
-	?>
+<div class="wrap-about">
+	<div class="about">
+		<div class="about__foto"><img src="img/about-foto.png" alt="about-foto"></div>
 
-</main><!-- #main -->
+		<div class="about__text">
+			<div class="about__title">О магазине</div>
+			<div class="about__text-text">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dolor mattis egestas arcu vitae, habitant elit. Egestas rhoncus, libero tincidunt integer euismod iaculis netus tristique. Quam sed ultricies molestie nullam. Erat non nunc id sed. Cursus lorem mattis sagittis dui ultricies felis erat. Proin bibendum aliquam,
+				</p>
+				<p>Tincidunt morbi nunc donec congue tincidunt est mi non semper. Id id integer amet sit at lectus turpis at sed. Nulla mauris curabitur diam, in. In dictumst diam id vel fames. Blandit vulputate vel mi nulla. Tempus consectetur donec gravida phasellus. Id justo, volutpat leo pellentesque. Aliquam ut ultricies vel sed. Aliquet quisque aliquam nam amet faucibus. Molestie tincidunt est etiam vitae orci enim, turpis. Mi nullam tincidunt elit ut.
+				</p>
+				<p>pellentesque purus. Elementum ac blandit odio massa vestibulum, erat pulvinar sed cursus. Aenean eget ullamcorper nisl amet. Nulla tincidunt sagittis, morbi sit rhoncus mattis porttitor ac. Elit ultricies sed tincidunt libero feugiat. Augue nulla purus elit ac, leo, eget.
+				</p>
+			</div>
+		</div>
 
-<?php
-get_sidebar();
-get_footer();
+	</div>
+</div>
+
+
+<script>
+	document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')
+</script>
+
+
+<?php get_footer() ?>
