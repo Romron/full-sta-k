54aa3378fff285b6831d80959f1d96243cb1b006
Romron <Romron@ukr.net>
2022-08-18 15:18:52 +0300

    heder and footer отображаются корректно

--- wordpress/ITDS_Agency_test/ITDS_Agency_test/wp-content/themes/itds_agency_test_2/functions.php
+++ wordpress/ITDS_Agency_test/ITDS_Agency_test/wp-content/themes/itds_agency_test_2/functions.php
@@ -16,6 +16,9 @@ require get_template_directory() . '/includes/widget-areas.php';
 require get_template_directory() . '/includes/enqueue-scripts-styles.php';
 require get_template_directory() . '/includes/helpers.php';
 
+require_once get_template_directory() . '/includes/class-tgm-plugin-activation.php';		// запрос необходимых плагинов
+require_once get_template_directory() . '/includes/redux-options.php';							// создать theme options panel
+
 require get_template_directory() . '/includes/custom-header.php';
 require get_template_directory() . '/includes/customizer.php';
 require get_template_directory() . '/includes/template-functions.php';
@@ -24,3 +27,16 @@ if (class_exists('WooCommerce')) {
 	require get_template_directory() . '/includes/woocommerce.php';
 	require get_template_directory() . '/woocommerce/wc-functions.php';
 }
+
+
+
+function ITDS_Agency_menus()
+{
+	register_nav_menus(array(
+		'heder_nav_menu-1' => esc_html__('Меню хедера 1'),
+		'heder_nav_menu-2' => esc_html__('Меню хедера 2'),
+		'footer_nav_menu-1' => esc_html__('Меню футера 1'),
+		'footer_nav_menu-2' => esc_html__('Меню футера категории')
+	));
+}
+add_action('after_setup_theme', 'ITDS_Agency_menus', 0);
--- wordpress/ITDS_Agency_test/ITDS_Agency_test/wp-content/themes/itds_agency_test_2/includes/theme-settings.php
+++ wordpress/ITDS_Agency_test/ITDS_Agency_test/wp-content/themes/itds_agency_test_2/includes/theme-settings.php
@@ -39,6 +39,49 @@ function itds_agency_test_2_setup()
 }
 add_action('after_setup_theme', 'itds_agency_test_2_setup');
 
+function ITDS_Agency_test_register_required_plugins()
+{
+   $plugins = array(
+
+      // This is an example of how to include a plugin bundled with a theme.
+      array(
+         'name'               => 'IITDS_Agency test core', // The plugin name.
+         'slug'               => 'itds_agency_test-core', // The plugin slug (typically the folder name).
+         'source'             => get_template_directory() . '/plugins/ITDS-Agency-test-core.zip', // The plugin source.
+         'required'           => true, // If false, the plugin is only 'recommended' instead of required.
+         'version'            => '1.0', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+         'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+         'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
+      ),
+      array(
+         'name'        => 'Gutenberg Template and Pattern Library & Redux Framework',
+         'slug'        => 'redux-framework',
+         'required'           => true,
+      ),
+      array(
+         'name'        => 'Regenerate Thumbnails',
+         'slug'        => 'regenerate-thumbnails',
+         'required'    => false,
+      ),
+   );
+
+   $config = array(
+      'id'           => 'ITDS_Agency_test',                 // Unique ID for hashing notices for multiple instances of TGMPA.
+      'default_path' => '',                      // Default absolute path to bundled plugins.
+      'menu'         => 'tgmpa-install-plugins', // Menu slug.
+      'has_notices'  => true,                    // Show admin notices or not.
+      'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
+      'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+      'is_automatic' => false,                   // Automatically activate plugins after installation or not.
+      'message'      => '',                      // Message to output right before the plugins table.
+
+
+   );
+
+   tgmpa($plugins, $config);
+}
+add_action('tgmpa_register', 'ITDS_Agency_test_register_required_plugins');
+
 function itds_agency_test_2_content_width()
 {
    $GLOBALS['content_width'] = apply_filters('itds_agency_test_2_content_width', 640);
