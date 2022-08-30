2837eecb33215d36d7a4012913c16233c44f6aba
Romron <Romron@ukr.net>
2022-08-30 15:01:01 +0300

    пагинация готова

--- wordpress/ITDS_Agency_test/ITDS_Agency_test/wp-content/themes/itds_agency_test_2/template-parts/product-card.php
+++ wordpress/ITDS_Agency_test/ITDS_Agency_test/wp-content/themes/itds_agency_test_2/template-parts/product-card.php
@@ -13,14 +13,14 @@ $product = $args['product'];
          </div>
 
       <?php else : ?>
-         <div class="wrap-rat-1" style="background-color:#fab2b2;">
-            <img src="img/icon-star.png" alt="star">
+         <div class="wrap-rat-1" style="background-color:#fcd9d9;">
+            <img src="<?php echo get_template_directory_uri() . '\assets\img\icon-star.png' ?>" alt="star">
             <div class="rating"><?php echo get_post_meta(get_the_ID(), 'rating', true); ?></div>
          </div>
       <?php endif; ?>
       <div class="wrap-rat-2">
-         <img src="img/icon-rat-hart.png" alt="">
-         <img src="img/icon-rat-diagram.png" alt="">
+         <img src="<?php echo get_template_directory_uri() . '\assets\img\icon-rat-hart.png' ?>" alt="">
+         <img src="<?php echo get_template_directory_uri() . '\assets\img\icon-rat-diagram.png' ?>" alt="">
       </div>
    </div>
 
--- wordpress/ITDS_Agency_test/ITDS_Agency_test/wp-content/themes/itds_agency_test_2/woocommerce/wc-functions.php
+++ wordpress/ITDS_Agency_test/ITDS_Agency_test/wp-content/themes/itds_agency_test_2/woocommerce/wc-functions.php
@@ -16,6 +16,7 @@ add_filter('woocommerce_enqueue_styles', '__return_empty_array');
 // отключить woocommerce_result_count
 remove_action('woocommerce_before_shop_loop', 'woocommerce_result_count', 20);
 
+// remove_action('woocommerce_after_shop_loop', 'woocommerce_pagination', 10);
 
 
 
@@ -32,8 +33,8 @@ function itds_agency_test_2_add_woocommerce_support()
 add_action('after_setup_theme', 'itds_agency_test_2_add_woocommerce_support', 20);
 
 // переделываю breadcrumbs 
-add_filter('woocommerce_breadcrumb_defaults', 'my_woocommerce_breadcrumbs');
-function my_woocommerce_breadcrumbs()
+add_filter('woocommerce_breadcrumb_defaults', 'itds_agency_test_2_woocommerce_breadcrumbs');
+function itds_agency_test_2_woocommerce_breadcrumbs()
 {
    return array(
       'delimiter'   => '&nbsp&nbsp > &nbsp&nbsp',
@@ -43,4 +44,18 @@ function my_woocommerce_breadcrumbs()
       'after'       => '',
       'home'        => _x('Главная', 'breadcrumb', 'woocommerce'),
    );
+}
+
+
+// переделываю пагинацию 
+add_filter('woocommerce_pagination_args', 'itds_agency_test_2_woocommerce_pagination_args_filter');
+function itds_agency_test_2_woocommerce_pagination_args_filter($array)
+{
+   $array = array(
+      'prev_text' => is_rtl() ? '&#10095;' : '&#10094;',
+      'next_text' => is_rtl() ? '&#10094;' : '&#10095;',
+      // 'type'      => 'list',
+   );
+
+   return $array;
 }
