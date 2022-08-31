5a33a493215a9e344e955a695fdc4c3a030f18cc
Romron <Romron@ukr.net>
2022-08-31 14:34:44 +0300

    что то

--- wordpress/ITDS_Agency_test/ITDS_Agency_test/wp-config.php
+++ wordpress/ITDS_Agency_test/ITDS_Agency_test/wp-config.php
@@ -79,7 +79,7 @@ $table_prefix = 'wp_itds_';
  *
  * @link https://ru.wordpress.org/support/article/debugging-in-wordpress/
  */
-define('WP_DEBUG', false);
+define('WP_DEBUG', true);
 
 /* Произвольные значения добавляйте между этой строкой и надписью "дальше не редактируем". */
 
--- wordpress/ITDS_Agency_test/ITDS_Agency_test/wp-content/themes/itds_agency_test_2/woocommerce/wc-functions.php
+++ wordpress/ITDS_Agency_test/ITDS_Agency_test/wp-content/themes/itds_agency_test_2/woocommerce/wc-functions.php
@@ -16,7 +16,9 @@ add_filter('woocommerce_enqueue_styles', '__return_empty_array');
 // отключить woocommerce_result_count
 remove_action('woocommerce_before_shop_loop', 'woocommerce_result_count', 20);
 
-// remove_action('woocommerce_after_shop_loop', 'woocommerce_pagination', 10);
+remove_action('woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+remove_action('woocommerce_sidebar', 'woocommerce_get_sidebar', 10);
+// remove_action('woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20);
 
 
 
@@ -58,4 +60,35 @@ function itds_agency_test_2_woocommerce_pagination_args_filter($array)
    );
 
    return $array;
+}
+
+// Ставим куки
+add_action('template_redirect', 'truemisha_recently_viewed_product_cookie', 20);
+function truemisha_recently_viewed_product_cookie()
+{
+
+   // если находимся не на странице товара, ничего не делаем
+   if (!is_product()) {
+      return;
+   }
+
+
+   if (empty($_COOKIE['woocommerce_recently_viewed_2'])) {
+      $viewed_products = array();
+   } else {
+      $viewed_products = (array) explode('|', $_COOKIE['woocommerce_recently_viewed_2']);
+   }
+
+   // добавляем в массив текущий товар
+   if (!in_array(get_the_ID(), $viewed_products)) {
+      $viewed_products[] = get_the_ID();
+   }
+
+   // нет смысла хранить там бесконечное количество товаров
+   if (sizeof($viewed_products) > 15) {
+      array_shift($viewed_products); // выкидываем первый элемент
+   }
+
+   // устанавливаем в куки
+   wc_setcookie('woocommerce_recently_viewed_2', join('|', $viewed_products));
 }
